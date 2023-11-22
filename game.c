739e6133da8a74428023ed6bcd7bf14a927364a3
David Ulrich <dulrich@c2r3p5.42vienna.com>
2023-11-22 14:15:53 +0100

    saving

--- game.c
+++ game.c
@@ -1,2 +1,25 @@
 #include "so_long.h"
 
+int	on_keypress(int keysym, t_data *data)
+{
+	(void)data;
+	printf("Pressed key: %d\\n", keysym);
+	return (0);
+}
+
+int main(void)
+{
+	void	*mlx_ptr;
+	void	*win_ptr;
+
+	mlx_ptr = mlx_init();
+	if (!mlx_ptr)
+		return (1);
+	win_ptr = mlx_new_window(mlx_ptr, 600, 400, "hello there");
+	if (!win_ptr)
+		return (free(mlx_ptr), 1);
+	mlx_destroy_window(mlx_ptr, win_ptr);
+	mlx_destroy_display(mlx_ptr);
+	free(mlx_ptr);
+	return (0);
+}
